34e0654fc9389c8eb0c1d316aaea98cd3f5f76cb
mzdness <mzdness@163.com>
2017-02-08 16:15:30 +0800

    v0.2 add hover preview
    ---
    consider done for now
    
    Signed-off-by: mzdness <mzdness@163.com>

--- addlink.js
+++ addlink.js
@@ -5,8 +5,21 @@ var idTopic = RegExp("www\.v2ex\.com/t/(\\d+)").exec(window.location.href)[1];
 
 var mapNameFloorId = {};
 var mapNameFloorElem = {};
+var hoverDisp = document.createElement('div');
+hoverDisp.style.display = 'none';
+hoverDisp.id = 'v2athelper_hover';
+hoverDisp.style.position = 'fixed';
+hoverDisp.style.border = '1px solid gray';
+hoverDisp.style.background = 'white'
+hoverDisp.classList.add('cell')
+document.body.append(hoverDisp);
 
-var url = window.location.pathname;
+function genJumpLink(atName, idTopic, jumpFloor){
+    var jumpLink = document.createElement('a');
+    jumpLink.text  = 'Jump';
+    jumpLink.href = "/t/"+ idTopic + "#" + jumpFloor;
+    return jumpLink;
+}
 
 for (var i=listFloor.length-1;i>=0;i--)
 {
@@ -14,21 +27,34 @@ for (var i=listFloor.length-1;i>=0;i--)
   for (var j=0;j<$listAt.length;j++)
   {
     let atName = $listAt[j].text;
-    var jumpLink = document.createElement('a');
-    jumpLink.text  = 'Jump';
-    jumpLink.href = "/t/"+ idTopic + "#" + mapNameFloorId[atName];
+    jumpLink = genJumpLink(atName, idTopic, mapNameFloorId[atName])
+
     $listAt[j].after(jumpLink)
     $listAt[j].after(' ')
-    let rpl_text = mapNameFloorElem[atName];
+
+    let rplContElm = mapNameFloorElem[atName];
     $(jumpLink).click(function() {
-      $(rpl_text).fadeTo('fast', 0.5, function(){
-        $(rpl_text).css("background-color","#E0E0FF")
+      $(rplContElm).fadeTo('fast', 0.5, function(){
+        $(rplContElm).css("background-color","#E0E0FF")
       });
-      $(rpl_text).fadeTo('fast', 1);
-      $(rpl_text).fadeTo('fast', 0.5, function(){
-        $(rpl_text).css("background-color","white");
+      $(rplContElm).fadeTo('fast', 1);
+      $(rplContElm).fadeTo('fast', 0.5, function(){
+        $(rplContElm).css("background-color","white");
       });
-      $(rpl_text).fadeTo('fast', 1);
+      $(rplContElm).fadeTo('fast', 1);
+    });
+
+    $(jumpLink).mouseenter(function(){
+      with ($(hoverDisp)){  
+        css("display","block"); 
+        css("left", event.clientX); 
+        css("top", event.clientY + 10); 
+      }
+      $(hoverDisp).html(rplContElm.parents('.cell').html())
+    });
+
+    $(jumpLink).mouseleave(function(){
+      $(hoverDisp).css("display","none"); 
     });
   }
 
@@ -36,3 +62,4 @@ for (var i=listFloor.length-1;i>=0;i--)
   mapNameFloorId[name] = $(listFloor[i]).attr("id");
   mapNameFloorElem[name] = $(listFloor[i]).find("div.reply_content");
 }
+
